f9ba7db5ce5c7a1f33cca7ee9726741655832a05
gangasaiusk <gangasai@uskcorp.in>
2025-11-26 11:26:38 +0530

    starting github actions

--- src/main/java/com/example/learning/LearningApplication.java
+++ src/main/java/com/example/learning/LearningApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
 	}
